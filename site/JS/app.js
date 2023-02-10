f18f1d242d6424ab28cee78ebb58b86d0975a613
R4F4EL-P <pereirasol7@gmail.com>
2023-02-10 17:02:21 +0000

    Finished Responsive nav Bar

--- site/JS/app.js
+++ site/JS/app.js
@@ -9,12 +9,12 @@ import { perspective } from "./libs/MV.js";
 
 
 const hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-menu');
 
 hamburger.addEventListener('click', function () {
     this.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
 });
-
-
 
 /** @type WebGLRenderingContext */
 let gl;
